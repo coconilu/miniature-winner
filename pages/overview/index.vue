d8f7f9ada50894f966edb3290391ae10575bf83b
Bayes <827130441@qq.com>
2024-10-01 06:47:16 +0800

    Refactor layout and add new pages
    - Refactor app.vue to use NuxtLayout and NuxtPage components for better organization and structure.
    - Add new layout component index.vue to create a flexible column layout.
    - Update nuxt.config.ts to include devtools configuration.
    - Add new pages: dashboard/index.vue and overview/index.vue.

---
+++ pages/overview/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="column overview-page">
+    <div class="overview-page-header">
+      <img
+        src="assets/imgs/overview_header.png"
+        alt=""
+        srcset=""
+        class="overview-page-header-pic"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.column {
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+}
+
+.overview-page-header {
+  margin: 0 auto;
+}
+
+.overview-page-header-pic {
+  height: 600px;
+}
+</style>
