92a8afcf41c798f10f849fda49433d56df406b60
Bayes <827130441@qq.com>
2024-10-02 06:59:34 +0800

    Refactor layout and add Vuetify components to dashboard page

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,3 +1,21 @@
 <template>
-  <div class="column">hello dashboard</div>
+  <v-layout class="rounded rounded-md">
+    <v-navigation-drawer>
+      <v-list>
+        <v-list-item title="Navigation drawer"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar title="Application bar"></v-app-bar>
+
+    <v-main class="d-flex align-stretch mt-6" style="min-height: 300px">
+      <v-expansion-panels>
+        <v-expansion-panel
+          title="Title"
+          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima"
+        >
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-main>
+  </v-layout>
 </template>
