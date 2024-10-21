87c5d147051b71ef8e4b1fb083dd495d50413803
Bayes <827130441@qq.com>
2024-10-21 09:18:11 +0800

    Refactor start.sh script to update file paths and add additional functionality

--- docker/start.sh
+++ docker/start.sh
@@ -10,8 +10,19 @@ ossutil cp oss://nuxt3-server-pack/output.tar.gz /demo-server/
 # unzip /demo-server/archive.zip -d /demo-server/
 tar -xzvf /demo-server/output.tar.gz -C /demo-server/
 
+# pm2 检查进程是否存在并停止该进程
+if pm2 list | grep -q "nuxt-demo-server"; then
+    pm2 stop nuxt-demo-server
+fi
+
+# 检查并创建目录 /demo-server/output
+mkdir -p /demo-server/output
+
+# 删除旧文件
+rm -rf /demo-server/output/*
+
 # 移动文件夹
-mv /demo-server/.output/* /demo-server/
+mv /demo-server/.output/* /demo-server/output/
 rmdir /demo-server/.output
 
 # 安装项目依赖
@@ -19,5 +30,5 @@ rmdir /demo-server/.output
 # npm install
 
 # 使用 pm2 启动项目
-pm2 start server/index.mjs --name "nuxt-demo-server"
+pm2 start output/server/index.mjs --name "nuxt-demo-server"
 pm2 logs
